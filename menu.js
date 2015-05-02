1552c13dd961a3d1d969b9fbd4d1ac6210407b42
raz <raz.kronenberg@gmail.com>
2015-05-03 00:53:25 +0300

    started switching to eastui doesnt display styled tree yet

---
+++ menu.js
@@ -0,0 +1,31 @@
+(function ($) {
+
+	// Draw tree
+	$(function () {
+		$.get('db.xml', function(d){
+			$('body').append('<dl />');
+		
+			var	html =  '<ul class="easyui-tree">';
+				html += '<span>' + $(d).attr("name") + '</span>';
+
+				$(d).children().each(function(){ // Curriculum
+					html += '<ul> <li data-options=\"state:\'closed\'\"> <span>' + $(this).attr("name") + '<span>';
+					
+					$(this).children().each(function(){ // lesson
+						html += '<ul> <li data-options=\"state:\'closed\'\"> <span>' + $(this).attr("title") + '<span>';
+						
+						$(this).children().each(function(){ // flashcard
+							if ($(this).attr("previewdisplay")){ 
+							
+								html += '<li><span>' + $(this).attr("symbol") + " : " + $(this).text() + '</span></li>';
+							}
+						});
+						html += '</ul> </li> '; 
+					});
+					html += '</ul> </li>';	
+				});
+				html += '</ul><br></br>';
+			$('dl').append($(html));
+		});
+	});
+})(jQuery);
