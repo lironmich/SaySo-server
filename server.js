df25d85ab5425c654e9ce0e8c9c90f5707c42350
raz <raz.kronenberg@gmail.com>
2015-05-04 02:57:44 +0300

    html parser with easyui

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ http.createServer(function(request, response) {
 	var filePath = '.' + request.url;
 	if (filePath == './')
 		filePath = './menu.html';
+	if (filePath == './testui')
+		filePath = './testui';
 	
 	fileShow(response, filePath);
 }).listen(8888);
@@ -34,7 +36,17 @@ function fileShow(response, filePath){
 		var menu = fs.readFileSync('menu.html', "utf-8");
 		var db =fs.readFileSync('db.xml', "utf-8");
 		
-		var content = parsexmldb(db, menu);
+		var content = parsexmldb(db, menu); // parsexmldb parseEasyUI
+		
+		response.writeHead(200, { 'Content-Type': contentType });
+		response.end(content, 'utf-8')
+		//console.log(content);
+	}
+	else if (filePath =='./testui'){
+		var menu = fs.readFileSync('menu.html', "utf-8");
+		var db =fs.readFileSync('db.xml', "utf-8");
+		
+		var content = parseEasyUI(db, menu); // parsexmldb parseEasyUI
 		
 		response.writeHead(200, { 'Content-Type': contentType });
 		response.end(content, 'utf-8')
@@ -55,29 +67,67 @@ function fileShow(response, filePath){
 	}
 }
 
+function parseEasyUI (db, menu){
+	var html = '   <div class="easyui-panel" style="padding:5px">	\n\
+        <ul class="easyui-tree">	\n\
+            <li>	\n\
+                <span>My Documents</span>	\n\
+                <ul>	\n\
+                    <li data-options=\"state:\'closed\'\">	\n\
+                        <span class="easyui-checkbox">Photos</span>	\n\
+                        <ul>	\n\
+                            <li>	\n\
+                                <span>Friend</span>	\n\
+                            </li>	\n\
+                            <li>	\n\
+                                <span>Wife</span>	\n\
+                            </li>	\n\
+                            <li>	\n\
+                                <span>Company</span>	\n\
+                            </li>	\n\
+                        </ul>	\n\
+                    </li>	\n\
+                    <li>	\n\
+                        <span>Program Files</span>	\n\
+                        <ul>	\n\
+                            <li>Intel</li>	\n\
+                            <li>Java</li>	\n\
+                            <li>Microsoft Office</li>	\n\
+                            <li>Games</li>	\n\
+                        </ul>	\n\
+                    </li>	\n\
+                    <li>index.html</li>	\n\
+                    <li>about.html</li>	\n\
+                    <li>welcome.html</li>	\n\
+                </ul>	\n\
+            </li>	\n\
+        </ul>	\n\
+    </div>	'
+	parsed = menu.replace('<div id="tree"></div>', html);
+	return (parsed);	
+}
+
 function parsexmldb(db, menu){
 	
 	var xmldoc = require('xmldoc');
 	var treemenu = new xmldoc.XmlDocument(db);
-	console.log("parsing xml pdb");
 	
-	var	html =  '<div id="menuTree" style="padding:5px">\n'
-		html += '<ul>\n'; 
+	var	html =  '<div  id="menuTree" class="easyui-panel" style="padding:5px" height="1000px" >\n'
+		html += '<ul class="easyui-tree"><li>\n'
 		html += '<span>' + treemenu.name + '</span>\n';
 		
 		//debugger;
 		treemenu.eachChild(function(Curriculum){
-			html += '<ul> <li  <span>' + Curriculum.attr.name + '</span>\n';
+			html += '<ul> <li data-options="state:\'open\'">  <span class="easyui-checkbox">' + Curriculum.attr.name + '</span>\n';
 			Curriculum.eachChild(function(Lesson){
-				html += '	<ul> <li> <span>' + Lesson.attr.name + '</span>\n';
+				html += '	<ul> <li data-options="state:\'closed\'"> <span class="easyui-checkbox">' + Lesson.attr.name + '</span>\n';
 				Lesson.eachChild(function(flashcard){
-					html += '		<ul> <li> <span>' + flashcard.attr.title + '</span><ul>\n';
+					html += '		<ul> <li data-options="state:\'closed\'"> <span class="easyui-checkbox">' + flashcard.attr.title + '</span><ul>\n';
 					flashcard.eachChild(function(face){
-						console.log(face.attr.fieldSymbol);
 						if (face.attr.previewDisplay == "true"){ 
-							html += '			<li><span>' + face.attr.symbol + " : " + face.val + '</span></li>\n';
+							html += '			<li>' + face.attr.symbol + " : " + face.val + '</li>\n';
 						} 
-					});
+					}); 
 					html += '			</ul></li></ul>  '; 
 				});
 				html += '		</li></ul>\n';
@@ -85,14 +135,13 @@ function parsexmldb(db, menu){
 			html += '	</li></ul>\n';
 		});
 		
-	html += '</ul></div>\n';
+	html += '</li></ul></div>\n';
 	parsed = menu.replace('<div id="tree"></div>', html);
 	return (parsed);	
 };
 
 // need to:
 
-// never show / load empty nodes!
 
 // get methodes
 // =============
